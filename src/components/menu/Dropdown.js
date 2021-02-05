aaceecc2a3f52c37e770e3101dce75230e436f32
Zoochini <maxime.bardiot@gmail.com>
2021-02-05 11:45:03 +0100

    Children submenu render

--- src/components/menu/Dropdown.js
+++ src/components/menu/Dropdown.js
@@ -1,10 +1,39 @@
 import React from "react";
 
+const Wrapper = ({ children }) => <div>{children}</div>;
+
 function Dropdown({ children, value }) {
+  console.log(children);
   return (
     <div id="dropdown" className="nav-item">
-      <button>{value}</button>
-      <div id="submenu">{children}</div>
+      <button>
+        {value}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="12"
+          viewBox="0 0 24 24"
+          className="mx-2"
+        >
+          <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+        </svg>
+      </button>
+      <div id="submenu">
+        {React.Children.map(children, (child) =>
+          React.cloneElement(child, child.props, [
+            child.props.children,
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+              <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+            </svg>,
+          ])
+        )}
+      </div>
     </div>
   );
 }
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -11,17 +11,55 @@ function Menu(props) {
       <Link to="/" className="nav-item">
         Accueil
       </Link>
-      <Dropdown value="DropdownTest">
+      <Dropdown value="Club">
         <Link to="/" className="nav-item">
-          Test
+          Historique de l'association
         </Link>
         <Link to="/bbb" className="nav-item">
-          Blabla
+          Le mot du président
         </Link>
         <Link to="/ccc" className="nav-item">
-          HELLO WORLD
+          Le conseil d'administration
         </Link>
       </Dropdown>
+      <Dropdown value="Equipes"></Dropdown>
+      <Dropdown value="Infos pratiques">
+        <Link to="/inscription" className="nav-item">
+          S'inscrire
+        </Link>
+        <Link to="/" className="nav-item">
+          Qui contacter ?
+        </Link>
+        <Link to="/" className="nav-item">
+          Synchroniser un calendrier
+        </Link>
+      </Dropdown>
+      <Dropdown value="Arbitrage">
+        <Link to="/" className="nav-item">
+          Devenez arbitre
+        </Link>
+        <Link to="/" className="nav-item">
+          Apprendre les gestes
+        </Link>
+        <Link to="/" className="nav-item">
+          Les arbitres du club
+        </Link>
+        <Link to="/" className="nav-item">
+          La vie d'arbitre
+        </Link>
+      </Dropdown>
+      <Link to="/actus" className="nav-item">
+        Actualités
+      </Link>
+      <Link to="/partenaires" className="nav-item">
+        Partenaires
+      </Link>
+      <Link to="/boutique" className="nav-item">
+        Boutique
+      </Link>
+      <Link to="/contact" className="nav-item">
+        Contact
+      </Link>
     </nav>
   );
 }

687e160b49b33eea41816aca2bdaba36022904d2
Zoochini <maxime.bardiot@gmail.com>
2021-03-16 16:44:22 +0100

    Inscription Functionnal

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
